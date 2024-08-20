3e887a8290ee952938ef78333b8f32b596293d7a
droplister <anderson@familymedia.org>
2024-08-19 22:48:48 -0400

    fdsfds

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 
+import { ApplicationLayout } from './application-layout'
 import Fathom from '@/components/fathom';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
           <Fathom />
-          {children}
+          <ApplicationLayout>{children}</ApplicationLayout>
       </body>
     </html>
   );
